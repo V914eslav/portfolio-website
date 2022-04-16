efa6a85bd5c7d52fbf584f8de2f853b1771a1784
V914eslav <v914-working@rambler.ru>
2022-04-16 17:08:31 +0300

    created the project folder structure

---
+++ src/containers/App/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import styles from "./App.module.css";
+// import cn from "classnames";
+
+function App() {
+  return <div className={styles.App}>123</div>;
+}
+
+export default App;
