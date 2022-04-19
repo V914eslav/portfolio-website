2f88ce6b91e7343d3943d8151c0965654cd4d224
V914eslav <v914-working@rambler.ru>
2022-04-19 13:07:17 +0300

    change header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 
 import styles from "./Header.module.css";
+
+const links = [
+  "обо мне",
+  "контакты",
+  "мой стек",
+  "мой проекты",
+  "отзывы",
+  "статьи",
+  "код сайта",
+];
 const Header = () => {
-  return <div className={styles.header}>Header</div>;
+  const navLinks = links.map((link, index) => {
+    return (
+      <a className={styles.link} key={index}>
+        {link}
+      </a>
+    );
+  });
+  const logo = <a className={styles.logo}>V914</a>;
+  return (
+    <div className={styles.header}>
+      {logo}
+      <nav className={styles.nav}>{navLinks}</nav>
+    </div>
+  );
 };
 
 export default Header;
