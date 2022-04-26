e439b28e59243d13b9555ea027974b67e93fad38
V914eslav <v914-working@rambler.ru>
2022-04-26 20:38:26 +0300

    change stub for MyStack

--- src/components/MyStack/MyStack.jsx
+++ src/components/MyStack/MyStack.jsx
@@ -3,28 +3,30 @@ import React from "react";
 // import cn from "classnames";
 import styles from "./MyStack.module.css";
 
-const links = [
-  "Html",
-  "Css, Scss, Less, Css in js",
-  "Javascript",
-  "JQuery",
-  "ReactJS",
-  "TypeScript",
-  "NodeJS",
-  "PHP",
-  "Laravel",
+const items = [
+  { name: "Html", text: "lorem3 " },
+  { name: "Css, Scss, Less, Css in js", text: "lorem3 " },
+  { name: "Javascript", text: "lorem3 " },
+  { name: "JQuery", text: "lorem3 " },
+  { name: "ReactJS", text: "lorem3 " },
+  { name: "TypeScript", text: "lorem3 " },
+  { name: "NodeJS", text: "lorem3 " },
+  { name: "PHP", text: "lorem3 " },
+  { name: "Laravel", text: "lorem3 " },
 ];
 const MyStack = () => {
-  const navLinks = links.map((link, index) => {
+  const listItems = items.map(({ name, text }, index) => {
     return (
-      <a className={styles.link} key={index}>
-        {link}
-      </a>
+      <div className={styles.item} key={index}>
+        <div>{name}</div>
+        <div>{text}</div>
+      </div>
     );
   });
   return (
     <div className={styles.wrapper}>
-      <nav className={styles.nav}>{navLinks}</nav>
+      <h2 className={styles.headers}>My Stack</h2>
+      <div className={styles.items}>{listItems}</div>
     </div>
   );
 };
