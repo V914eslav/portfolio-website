7dcd1e69d009e0d4bf60feef07fac813b8ba4fa5
V914eslav <v914-working@rambler.ru>
2022-05-15 20:27:19 +0300

    change Contacts

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 
 export default class Contacts extends Component {
   render() {
-    return <div>Contacts</div>;
+    return (
+      <div>
+        <div>E-mail: X1X2X3X4X5X@yahoo.com</div>
+        <div>Phone: +0 000 000 00 00</div>
+        <div>Telegram: V914eslav</div>
+      </div>
+    );
   }
 }
