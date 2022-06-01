efff1cb52d6acc012821aa618832ec69ee1385fd
V914eslav <v914-working@rambler.ru>
2022-06-01 12:46:08 +0300

    change Projects

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ProjectCard() {
+  return (
+    <div className="card">
+      <img src="#" alt="card img" />
+      <h4 className="title">name project</h4>
+      <div className="description">description</div>
+    </div>
+  );
+}
+
+export default ProjectCard;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
+import ProjectCard from "../ProjectCard";
+
+import styles from "./Projects.module.css";
 
 export default class Projects extends Component {
   render() {
-    return <div>Projects</div>;
+    return (
+      <div className={styles.cards}>
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </div>
+    );
   }
 }
