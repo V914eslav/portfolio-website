ec46c95f0c798e25cf78c7909f9d70f0cf056239
V914eslav <v914-working@rambler.ru>
2022-06-06 19:39:35 +0300

    change stackCard

--- src/components/Stack/Stack.jsx
+++ src/components/Stack/Stack.jsx
@@ -1,9 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import StackCard from "../StackCard/StackCard";
 
+import styles from "./Stack.module.css";
 export default class Stack extends Component {
   render() {
     return (
-      <div>Stack</div>
-    )
+      <div className={styles.stack}>
+        <StackCard />
+        <StackCard />
+        <StackCard />
+        <StackCard />
+      </div>
+    );
   }
 }
--- src/components/StackCard/StackCard.jsx
+++ src/components/StackCard/StackCard.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function StackCard() {
-  return <div>StackCard</div>;
+  return (
+    <div className="card">
+      <img src="#" alt="card img" />
+      <h4 className="title">name stack</h4>
+      <div className="description">description</div>
+    </div>
+  );
 }
 
 export default StackCard;
